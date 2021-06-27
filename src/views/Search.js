9466e1d1dce95abb3ae91fff61c0318c00ccfa90
selimfelex <seelimfelex@hotmail.com>
2021-06-27 15:12:39 +0200

    add render fuction to search page

--- src/views/Search.js
+++ src/views/Search.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Book from '../BooksAPI';
+import Book from '../components/Book';
 import { getAll } from '../BooksAPI';
 import {search} from '../BooksAPI';
 
@@ -49,20 +49,39 @@ class Search extends Component {
             <div className="search-books-bar">
                 <Link className="close-search" to={"/"}>Back</Link>
                 <div className="search-books-input-wrapper">
-                    {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                    <input type="text" placeholder="Search by title or author" />
+                    <input type="text" placeholder="Search by title or author"onChange={this.handleChange}
+                            value={this.state.query} />
 
                 </div>
             </div>
             <div className="search-books-results">
-                <ol className="books-grid"></ol>
+                <ol className="books-grid">
+                {this.state.books.length > 0 &&
+                            this.state.books.map(book => {
+                            const foundShelf = this.props.books.find(
+                                searchBook => searchBook.id === book.id
+                            );
+
+                            if (foundShelf) {
+                                book.shelf = foundShelf.shelf;
+                            } else {
+                                book.shelf = "none";
+                            }
+
+                            return (
+                                <Book
+                                    key={book.id}
+                                    {...book}
+                                    moveBook={this.props.moveBook}
+                                />
+                            );
+                            })}
+                        {this.state.books.length === 0 && (
+                            <h1 style={{ textAlign: "center"}}> No results found </h1>
+                        )}
+
+                </ol>
             </div>
         </div>);
     }
